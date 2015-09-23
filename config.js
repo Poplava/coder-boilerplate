32ed04399b3161c81c5a8f5fe4fc7e83b7ff021a
Igor <igor.poplava@gmail.com>
2015-09-24 00:06:50 +0300

    watch

--- config.js
+++ config.js
@@ -1,22 +1,35 @@
 module.exports = {
+  src: {
+    jade: 'src/html/*.jade',
+    jadeComponents: 'src/components/**/*.jade',
+    less: 'src/css/*.less',
+    lessComponents: 'src/components/**/*.less',
+    sprite: 'src/sprite-source/*.png',
+    spriteLess: 'src/images/sprite/sprite.less'
+  },
+
+  dest: {
+    less: '.',
+    jade: '.',
+    sprite: 'src/images/sprite'
+  },
+
   less: {
-    src: 'src/css/*.less',
-    dest: '.',
-    less: {
-      relativeUrls: true,
-      pretty: true
-    },
-    autoprefixer: {
-      browsers: ['last 2 versions'],
-      cascade: false
-    }
+    relativeUrls: true,
+    pretty: true
+  },
+
+  autoprefixer: {
+    browsers: ['last 2 versions'],
+    cascade: false
   },
 
   jade: {
-    src: 'src/html/*.jade',
-    dest: '.',
-    jade: {
-      pretty: true
-    }
+    pretty: true
+  },
+
+  spritesmith: {
+    imgName: 'sprite.png',
+    cssName: 'sprite.less'
   }
 };
