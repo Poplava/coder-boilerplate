22a2ccc12a9b4f7e432b1d18a0a2637051520c87
Igor <igor.poplava@gmail.com>
2015-09-23 23:48:31 +0300

    less & jade

--- config.js
+++ config.js
@@ -1,1 +1,22 @@
-module.exports = {};
+module.exports = {
+  less: {
+    src: 'src/css/*.less',
+    dest: '.',
+    less: {
+      relativeUrls: true,
+      pretty: true
+    },
+    autoprefixer: {
+      browsers: ['last 2 versions'],
+      cascade: false
+    }
+  },
+
+  jade: {
+    src: 'src/html/*.jade',
+    dest: '.',
+    jade: {
+      pretty: true
+    }
+  }
+};
--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,27 @@
-module.exports = {};
+var gulp = require('gulp'),
+  config = require('./config'),
+  less = require('gulp-less'),
+  jade = require('gulp-jade'),
+  autoprefixer = require('gulp-autoprefixer'),
+  livereload = require('gulp-livereload');
+
+gulp.task('less', lessTask);
+gulp.task('jade', jadeTask);
+
+function lessTask(done) {
+  return gulp.src(config.less.src, { base: '.' })
+    .pipe(less(config.less.less))
+    .on('error', done)
+    .pipe(autoprefixer(config.less.autoprefixer))
+    .on('error', done)
+    .pipe(gulp.dest(config.less.dest))
+    .pipe(livereload());
+}
+
+function jadeTask(done) {
+  return gulp.src(config.jade.src, { base: '.' })
+    .pipe(jade(config.jade.jade))
+    .on('error', done)
+    .pipe(gulp.dest(config.jade.dest))
+    .pipe(livereload());
+}
